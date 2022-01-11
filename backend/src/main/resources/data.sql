b8e4050cc35a00a58ad34e18109475ca0a5c8f44
Fabricio Araújo <fabriciofa@gmail.com>
2022-01-11 15:15:00 -0300

    course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,4 @@
+/* USER and ROLE */
 INSERT INTO tb_user(name, email, password) VALUES('Alex Brown', 'alex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 INSERT INTO tb_user(name, email, password) VALUES('Bob Brown', 'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 INSERT INTO tb_user(name, email, password) VALUES('Maria Green', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
@@ -13,3 +14,5 @@ INSERT INTO tb_user_role(user_id, role_id) VALUES(3, 1);
 INSERT INTO tb_user_role(user_id, role_id) VALUES(3, 2);
 INSERT INTO tb_user_role(user_id, role_id) VALUES(3, 3);
 
+/* COURSE */
+INSERT INTO tb_course(name, img_uri, img_gray_uri) VALUES('Bootcamp HTML', 'https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d', 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg')
